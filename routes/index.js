77c85996d603cc947b231b555cc7eb1310844cec
badunk <baduncaduncan@gmail.com>
2016-01-09 19:43:32 -0800

    added sendgrid

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var nodemailer = require('nodemailer');
+var sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 // var receivers = require('../receivers');
 var receivers = ['alisonnganyc@gmail.com']
-
-var transporter = nodemailer.createTransport('smtps://tunglinbuddhistgroups@gmail.com:tunglin01@smtp.gmail.com');
 
 router.post('/submit', function(req, res, next){
   var body = req.body.header + '\n\n';
@@ -13,19 +11,18 @@ router.post('/submit', function(req, res, next){
   }
   body += '\n' + req.body.footer
   // setup e-mail data with unicode symbols
-  var mailOptions = {
-      from: 'Tunglin Buddhist Group <tunglinbuddhistgroups@gmail.com>', // sender address
-      to: receivers, // list of receivers
-      subject: req.body.subject, // Subject line
-      text: body, // plaintext body
-  };
+  sendgrid.send({
+    to:       receivers,
+    from: 'Tunglin Buddhist Group <tunglinbuddhistgroups@gmail.com>',
+    subject: req.body.subject,
+    text: body,
+  }, function(err, json) {
+    if (err) return console.error(err);
 
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, function(error, info){
-      if(error) return console.log(error);
-      console.log('Message sent: ' + info.response);
-      res.sendStatus(200)
+    console.log('Message sent: ' + info.response);
+    res.sendStatus(200)
   });
+
 })
 
 module.exports = router;

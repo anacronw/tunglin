28a602637c720c0edb84bfa408c018534d961caa
badunk <baduncaduncan@gmail.com>
2016-01-09 19:28:17 -0800

    added basic auth

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var basic = require('basic-auth-connect');
 
 var routes = require('./routes/index');
 
@@ -12,6 +13,7 @@ var app = express();
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(basic('alison', 'Bin$tix'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
